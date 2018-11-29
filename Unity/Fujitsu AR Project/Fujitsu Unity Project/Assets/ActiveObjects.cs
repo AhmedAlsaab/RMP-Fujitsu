444159c84114bf5ac895c9ffa1c332258d1acd36
Ahmed Alsaab <ahmed_alsaab@hotmail.com>
2018-11-29 20:06:01 +0000

    The introduction screen now functions correctly. Project buttons now navigate to the correct view by setting them active. Active Objects script sets the two different project-proccesses off when the application starts as buttons set them active now

--- Unity/Fujitsu AR Project/Fujitsu Unity Project/Assets/ActiveObjects.cs
+++ Unity/Fujitsu AR Project/Fujitsu Unity Project/Assets/ActiveObjects.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class ActiveObjects : MonoBehaviour {
     public GameObject Popup;
     public GameObject IntroScreen;
+    public GameObject CampaignProcesses;
+    public GameObject FairProcesses;
+
 
 	// Use this for initialization
 	void Start () {
         Popup.SetActive(false);
         Popup.transform.SetAsLastSibling();
 
-
         IntroScreen.SetActive(true);
+
+        CampaignProcesses.SetActive(false);
+        FairProcesses.SetActive(false);
+
+
     }
 
     // Update is called once per frame
