4014c1333354d3666c3e973bae0f49920f92ae9f
Ahmed Alsaab <ahmed_alsaab@hotmail.com>
2018-11-29 19:34:03 +0000

    Developed a project selection panel for the given department. It contains two buttons that represent projects within the targetted department. These are interractable but not for each project

--- Unity/Fujitsu AR Project/Fujitsu Unity Project/Assets/ActiveObjects.cs
+++ Unity/Fujitsu AR Project/Fujitsu Unity Project/Assets/ActiveObjects.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class ActiveObjects : MonoBehaviour {
     public GameObject Popup;
+    public GameObject IntroScreen;
 
 	// Use this for initialization
 	void Start () {
         Popup.SetActive(false);
         Popup.transform.SetAsLastSibling();
 
+
+        IntroScreen.SetActive(true);
     }
 
     // Update is called once per frame
