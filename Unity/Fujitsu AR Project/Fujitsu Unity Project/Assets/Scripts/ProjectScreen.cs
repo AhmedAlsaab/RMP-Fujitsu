99ed8572d7497e8dcfadd1ee86dbedf9bd5d2b77
Ahmed Alsaab <ahmed_alsaab@hotmail.com>
2018-12-05 03:14:20 +0000

    Adapted the ProcessDetailInserter script into the ProjectScreen script. This script now fetches project titles from the passed in API and stores the values found into child-objects of the parent component. It also creates the total amount of projects found from the given API link.

--- Unity/Fujitsu AR Project/Fujitsu Unity Project/Assets/Scripts/ProjectScreen.cs
+++ Unity/Fujitsu AR Project/Fujitsu Unity Project/Assets/Scripts/ProjectScreen.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections.Generic;
-using System.Collections;
-using Vuforia;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.IO;
+using System;
+using System.Text;
+using SimpleJSON;
+using Vuforia;
 
 public class ProjectScreen : MonoBehaviour, ITrackableEventHandler {
 
+    private string baseURL = "https://live.runmyprocess.com/";
     public GameObject MarketingCanvas;
-
     private TrackableBehaviour trackableBehaviour;
-
     private bool ShowGUIButton = false;
     private Rect ButtonRect = new Rect(50, 50, 120, 60);
+    private int projectLimit = 1;
+    public GameObject projectPrefab;
+    public GameObject projectParent;
+    private int processAligner = 0;
 	
 	// Use this for initialization
 	void Start () {
@@ -20,13 +29,65 @@ public class ProjectScreen : MonoBehaviour, ITrackableEventHandler {
             trackableBehaviour.RegisterTrackableEventHandler(this);
         }
 
-		
+		StartCoroutine(GetText());
 	}
+
+ IEnumerator GetText() {
+		var username = "cristiano.bellucci.fujitsu+cardiffadmin@gmail.com";
+		var password = "Millennium";
+
+
+		var credentials = Convert.ToBase64String (Encoding.ASCII.GetBytes (username + ":" + password)); // FROM TEAM 7
+		WWWForm form = new WWWForm();  // FROM TEAM 7
+		var headers = form.headers;  // FROM TEAM 7
+		headers ["Authorization"] = "Basic " + credentials;  // FROM TEAM 7
+		headers ["Accept"] = "application/json";
+
+		WWW www = new WWW("https://live.runmyprocess.com/config/112761542179152739/user/993079/project/?filter=NAME&operator=CONTAINS&value=CWL$_$market&method=GET&P_rand=17306", null, headers);
+
+		yield return www;
+
+
+		// Preparation for process generation.
+		var jsonObject =  JSON.Parse(www.text);
+		var arrayOfProcesses = jsonObject["feed"]["entry"].AsArray;
+
+		int i = 0;
+
+        // Getting the process report.
+        foreach (var arrayItem in arrayOfProcesses.Values)
+        {
+            if (i < projectLimit)
+            {
+
+                GameObject createdProject = createProject(arrayItem["title"].Value);
+
+                // Adding in title for process.
+                createdProject.GetComponentsInChildren<Text>()[0].text = arrayItem["title"].Value;
+
+            }
+        }
+	}
+	
 
     void Update()
     {
                         // MarketingCanvas.SetActive(false);
 
+    }
+
+    GameObject createProject(string projectTitle)
+    {
+        GameObject individualProject = Instantiate(projectPrefab);
+        individualProject.transform.SetParent(projectParent.transform, false);
+
+        individualProject.transform.Translate(0, processAligner, 0);
+
+        processAligner -= 75;
+
+        individualProject.GetComponentsInChildren<Text>()[0].text = projectTitle;
+
+        return individualProject;
     }
 
     public void OnTrackableStateChanged(
